c09e6210ff1619b4551fa2754dcb09756e9e7663
Robert Krahn <robert.krahn@gmail.com>
2019-08-12 01:05:08 +0200

    help

--- src/org_analyzer/view/help_view.cljs
+++ src/org_analyzer/view/help_view.cljs
@@ -6,21 +6,34 @@
     [:h2 "About"]
 
     [:p "Org Analyzer creates an interactive visualization of org-mode
-   time-tracking data (via org-clock-in). It parses org files and extracts
-   CLOCK: timestamps. This is the underlying data displayed here."]
+time-tracking data (via org-clock-in). It parses org files and extracts CLOCK:
+timestamps. This is the underlying data displayed here."]
 
-    [:h3 "Controls"]
+    [:h3 "Search"]
 
-    [:p "The app has four main panels:"
-     [:ol
-      [:li "search bar"
-       [:p "foo"]]
-      [:li "calendar"]]]
+    [:p "The terms entered in the search panel will by default be matched with the full section names of clock entries. A space between two words means " [:i "and"] ". If groups of words are surrounded by quotation marks the exact phrase must match. Example:"
+     [:pre "* some entry                                             :tag1:
+** goes here                                             :tag2:
+:LOGBOOK:
+CLOCK: [2019-08-10 Sat 13:17]--[2019-08-10 Sat 14:09] =>  0:52
+:END:
+"]
+     [:span "The entry above would be found if we would enter " [:i "goes"] " or " [:i "some here"] " or " [:i "\"some entry\""] "."]
 
-    [:p "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"]
-    [:p "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"]
-    [:p "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"]
-    [:p "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"]]
+     [:p "It is also possible to search for tags by prepending a search term with +. For example " [:i "+tag2"] " would match the entry above, so would " [:i "+tag1"] ". When a - is prepended an entry with such a tag will not match."]]
+
+    [:h3 "Calendar"]
+    [:p "You can select individual days in the calendar by clicking on the day boxes. When you press SHIFT and click you will add to the selection. ALT and click removes from the selection. You can also click on the top of weeks to select weeks or on month, to select them. Clicking and dragging will bring up a rectangular selection that allows you to select multiple days at once."]
+
+    [:h3 "Shortcuts"]
+    [:span [:code "[CTRL + s]"] " focus search input"]
+    [:br]
+    [:span [:code "[CTRL + a]"] " select all days in calendar"]
+
+    [:h3 "Contact"]
+    [:p "If you have questions, issues, or need a developer, please get in touch: " [:a {:href "mailto:robert@kra.hn"} "robert@kra.hn"] "."]
+
+]
    [:button.close.material-button
     {:on-click close-help-fn}
     [:i.material-icons "close"]]])
